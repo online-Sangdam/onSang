c875298f9cbed2d4fde59c6678aeb15acbd71914
jeong011010 <jeong011010@naver.com>
2023-05-22 14:42:15 +0000

    update navBar

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -6,9 +6,11 @@ function NavBar() {
   return (
     <div className={styles.NavBody}>
       <div className={styles.SideNav}>
-        <Sidebar width={320} />
-        <div className={styles.Title}>
-          <Link to={"/"}>온상</Link>
+        <div className={styles.leftElement}>
+          <Sidebar width={320} />
+          <div className={styles.Title}>
+            <Link to={"/"}>온상</Link>
+          </div>
         </div>
         <div className={styles.d}>
           <Link data-cy="login-button" className={styles.d} to="/auth/login?next=/">로그인/가입</Link>

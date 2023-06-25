5b1fdfb3c2f53331e6d2b9823b53654814df8a95
jeong011010 <jeong011010@naver.com>
2023-06-25 11:16:31 +0000

    로그인창 수정

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,10 +1,24 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import styles from "./NavBar.module.css";
+
 
 import Sidebar from "./SideBar"; // 왼쪽 메뉴바 components
 // 상단 메뉴바에 왼쪽 사이드 메뉴바 버튼 있어서 여기로 import 함
 
 function NavBar() { // 상단 메뉴바
+
+  const [inLogin, setinLogin] = useState(true);
+  const location = useLocation();
+  let url;
+  useEffect(() => {
+    console.log(location);
+    url = location.pathname;
+    if (url === "/onSang/Login")
+      setinLogin(true);
+    else setinLogin(false);
+  }, [location])
+
 
   const Style = {
     textDecoration: "none",
@@ -21,9 +35,13 @@ function NavBar() { // 상단 메뉴바
             <Link to={"/onSang"} style={Style}>온상</Link> {/*메인 페이지 이동*/}
           </div>
         </div>
-        <div className={styles.Login}>
-          <Link to={"/onSang/Login"} style={Style}>로그인</Link> {/*로그인 페이지 이동*/}
-        </div>
+        {
+          inLogin ? <div></div> :
+            <div className={styles.Login}>
+              <Link to={"/onSang/Login"} style={Style}>로그인</Link> {/*로그인 페이지 이동*/}
+            </div>
+        }
+
       </div>
     </div>
   )
--- my-app/src/router/Login.js
+++ my-app/src/router/Login.js
@@ -1,17 +1,40 @@
-
+import { useEffect } from "react";
 import styles from "./Login.module.css"
 
+function Kakao() {
+  useEffect(() => {
+
+    const script = document.createElement("script")
+    script.src = "https://developers.kakao.com/sdk/js/kakao.js"
+
+    window.Kakao.init("4a0465204124cb18162eedbb5ffd63a3");
+
+    function kakaoLogin() {
+      window.Kakao.Auth.login({
+        scope: 'profile_nickname, account_email, gender',
+        success: function(authObj) {
+          console.log(authObj);
+          window.Kakao.API.request({
+            url: '/v2/user/me',
+            success: res => {
+              const kakao_account = res.kakao_account;
+              console.log(kakao_account);
+            }
+          });
+        }
+      });
+    }
+    document.body.appendChild(script)
+  })
+}
 
 function Login() {
-
-
-
 
   return (
     <div className={styles.parent}>
       <div className={styles.Banner}>
-        <div style={{paddingTop: "70px"}}>
-          <div>로그인 창</div>
+        <div style={{ paddingTop: "70px" }}>
+          <a href="javascript:kakaoLogin();"><img src="https://www.gb.go.kr/Main/Images/ko/member/certi_kakao_login.png" /></a>
           <div>Spring 공부 후 개발 및 연동 예정</div>
         </div>
       </div>

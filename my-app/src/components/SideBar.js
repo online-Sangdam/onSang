cee0416456d2500eb411b473b94d42709a57ff77
jeong011010 <jeong011010@naver.com>
2023-06-30 12:16:20 +0000

    반응형 웹 수정 (gpt의 힘을 빌린..)

--- my-app/src/components/SideBar.js
+++ my-app/src/components/SideBar.js
@@ -1,17 +1,11 @@
-import React, { useRef, useState } from "react";
-import { Link } from "react-router-dom"; 
-
+import React, { useRef, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import styles from "./SideBar.module.css";
-
 import sideBtn from "../img/SideBtn.png"; // 버튼 이미지
 
-
-
-const Sidebar = ({ width = 280, children }) => { // NavBar에서 SideBar로 props 받기
-
+const Sidebar = ({ width = 280, children }) => {
   const [xPosition, setX] = useState(width);
   const side = useRef();
-
   const menus = [
     { id: "1", name: "Select", path: "/Select" },
     { id: "2", name: "MENU 2", path: "/" },
@@ -26,32 +20,67 @@ const Sidebar = ({ width = 280, children }) => { // NavBar에서 SideBar로 prop
     }
   };
 
-  return ( // 이쪽은 크게 몰라도 됨
-    <div>
-      <img src={sideBtn} alt="contact open button" className={styles.button} onClick={() => toggleMenu()} />
-      <div className={styles.container}>
-        <div ref={side} className={styles.sidebar} style={{ width: `${width}px`, height: '100%', transform: `translatex(${-xPosition}px)` }}>
+  // 화면 크기 변화에 따라 SideBar 닫기
+  const handleResize = () => {
+    const screenWidth = window.innerWidth;
+    if (screenWidth > 768) {
+      setX(width);
+    } else {
+      // If the screen size is less than or equal to 768px, set the Sidebar width to a smaller value
+      setX(180); // You can adjust the value as per your requirement
+    }
+  };
 
-          <div className={styles.exitButton} onClick={() => toggleMenu()}>X</div>
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div>
+      <img
+        src={sideBtn}
+        alt="contact open button"
+        className={styles.button}
+        onClick={() => toggleMenu()}
+      />
+      <div className={styles.container}>
+        <div
+          ref={side}
+          className={styles.sidebar}
+          style={{
+            width: `${width}px`, // Update the width with xPosition
+            height: "100%",
+            transform: `translatex(${-xPosition}px)`,
+          }}
+        >
+          <div className={styles.exitButton} onClick={() => toggleMenu()}>
+            X
+          </div>
           <div className={styles.content}>
             {menus.map((menu) => {
               return (
-                <Link 
+                <Link
                   key={menu.id}
                   onClick={() => toggleMenu()}
-                  style={{ color: "white", textDecoration: "none", paddingTop: "40px" }}
+                  style={{
+                    color: "white",
+                    textDecoration: "none",
+                    paddingTop: "40px",
+                  }}
                   to={`/onSang${menu.path}`}
-                  >{menu.name}</Link>
-              )
+                >
+                  {menu.name}
+                </Link>
+              );
             })}
-
           </div>
-
         </div>
       </div>
     </div>
   );
 };
-
 
 export default Sidebar;
--- my-app/src/router/Home.js
+++ my-app/src/router/Home.js
@@ -1,20 +1,16 @@
-
-// react 기본 파일
-
-import styles from "./Home.module.css"
+import styles from "./Home.module.css";
 
 function Home() {
-
   return (
     <div className={styles.parent}>
       <div className={styles.Banner}>
-        <div style={{ paddingTop: "145px" }}>
-          <div style={{ fontSize: "50px", fontWeight: "bold" }}>온상</div>
-          <div style={{ fontSize: "20px", paddingTop: "30px" }}>여러분의 온라인 상담을 해드립니다.</div>
+        <div className={styles.Block}>
+          <div className={styles.Title}>온상</div>
+          <div className={styles.Description}>여러분의 온라인 상담을 해드립니다.</div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Home;
